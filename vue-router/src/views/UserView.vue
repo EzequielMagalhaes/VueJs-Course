13c63a2bdda3d06712cfccb0b4fc55e43388fba9
Ezequiel Magalhaes <zekes_243@hotmail.com>
2024-12-24 11:43:09 -0300

    Salvando conteudo

---
+++ vue-router/src/views/UserView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Seu nome é {{ nome }} e isso é um teste!</h1>
+    <p>Teste bem sucedido!</p>
+    <input type='text' name='Email' id='email' placeholder='exemplo@exemplo.com.br'><br>
+    <input type='text' name='Senha' id='senha' placeholder='Exemplo@123'><br>
+    <input type='submit' name='Enviar' id='enviar'>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            nome:'Ezequiel'
+        }   
+    }
+}
+</script>
