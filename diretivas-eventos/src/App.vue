a7e6fcce26857dd8a105c1592dec2ee490ca9400
Ezequiel Magalhaes <zekes_243@hotmail.com>
2024-12-24 18:34:31 -0300

    Salvando conteudo

---
+++ diretivas-eventos/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <ClienteTeste/>
+</template>
+
+<script>
+import ClienteTeste from './components/Cliente.vue'
+export default {
+  name: 'App',
+  components: {
+    ClienteTeste
+  }
+}
+</script>
+
+<style>
+</style>
