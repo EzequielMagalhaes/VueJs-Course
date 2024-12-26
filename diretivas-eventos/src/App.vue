95f0ea2c69d329dce8fbf764749fdc30f8507d02
Ezequiel Magalhaes <zekes_243@hotmail.com>
2024-12-26 14:49:25 -0300

    Salvando conteudo

--- diretivas-eventos/src/App.vue
+++ diretivas-eventos/src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <ClienteTeste/>
+  <input type="text" v-model="dadosDoCliente.nome">
+  <input type="text" v-model="dadosDoCliente.email">
+  <PropsTeste :nome="dadosDoCliente.nome" :email='dadosDoCliente.email' :idade='dadosDoCliente.idade'/>
+  <PropsTeste nome='Ezequiel Magalhães' email='zekiel.dev@gmail.com' idade='21'/>
+  <PropsTeste nome='Nathalia Silvana Tomm de Souza' email='natthytdz@gmail.com' idade='20'/>
+  <PropsTeste nome='Testando props' email='teste@teste.com' idade='teste'/>
 </template>
 
 <script>
-import ClienteTeste from './components/Cliente.vue'
+import PropsTeste from './components/Props.vue'
 export default {
   name: 'App',
+  data(){
+    return{
+      dadosDoCliente:{
+        nome:'Anakin Skywalker',
+        email:'anksky@gmail.com',
+        idade:'200'
+      }
+    }
+  },
   components: {
-    ClienteTeste
+    PropsTeste
   }
 }
 </script>
-
 <style>
 </style>
---
+++ diretivas-eventos/src/components/Props.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id='props'>
+        <h4>Nome : {{ nome }}</h4><hr>
+        <p>Descrição: {{ descricao }}</p><hr>
+        <p>Número: {{ numero }}</p>
+        <p>Email: {{ email }}</p>
+        <p>Idade: {{ idade }}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name:'PropsTeste',
+    data(){
+        return {
+            //nome: 'Ezequiel', // Se deixar aqui, vai causar erro por duplicade com a props instanciada abaixo. 
+            descricao: 'Descrição de teste',
+            numero:'(xx) x xxxx-xxxx',
+            //email: 'exemlo@exemplo.com.br',
+            //idade: 0
+        }
+    },
+    props:{
+        nome: String, //O atributo tem que ser algum tipo Javascript (String, Number, Array, Object,etc) 
+        email: String,
+        idade: Number
+    }
+}
+</script>
+
+<style scoped>
+    #props{
+        background-color: grey;
+        padding: 2%;
+        margin-top: 2%;
+    }
+</style>
